6fb08fcc22e3f51609a9d1cb4142765c49395f85
achrafRejouan <achrafrejouan@gmail.com>
2025-04-11 22:36:40 +0100

    TP4: Implement article management features: add CRUD operations, update templates, and configure database connection

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,18 +2,112 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class IndexController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
-    public function home(): Response
+    #[Route('/', name: 'article_list')]
+    public function home(EntityManagerInterface $em): Response
     {
-        $articles = ['Article 1', 'Article 2', 'Article 3'];
+        $articles = $em->getRepository(Article::class)->findAll();
         return $this->render('articles/index.html.twig', [
-            'articles' => $articles,
+            'articles' => $articles
         ]);
+    }
+
+    #[Route('/article/save', name: 'addArticle')]
+    public function save(EntityManagerInterface $em): Response
+    {
+        $article = new Article();
+        $article->setNom('Article 1');
+        $article->setPrix(1000);
+
+        $em->getRepository(Article::class);
+        $em->persist($article);
+        $em->flush();
+
+        return new Response('Article enregistré avec id ' . $article->getId());
+    }
+
+    #[Route('/article/new', name: 'new_article', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $em): Response
+    {
+        $article = new Article();
+        $form = $this->createFormBuilder($article)
+            ->add('nom', TextType::class)
+            ->add('prix', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Créer'))
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->persist($article);
+            $em->flush();
+            return $this->redirectToRoute('article_list');
+        }
+        return $this->render('articles/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/article/{id}', name: 'article_show')]
+    public function show($id, EntityManagerInterface $em): Response
+    {
+        $article = $em->getRepository(Article::class)->find($id);
+        if (!$article) {
+            throw $this->createNotFoundException('Article non trouvé');
+        }
+        return $this->render('articles/show.html.twig', array(
+            'article' => $article
+        ));
+    }
+
+    #[Route('/article/edit/{id}', name: 'article_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, $id, EntityManagerInterface $em): Response
+    {
+        $article = new Article();
+        $article = $em->getRepository(Article::class)->find($id);
+        if (!$article) {
+            throw $this->createNotFoundException('Article non trouvé');
+        }
+
+        $form = $this->createFormBuilder($article)
+            ->add('nom', TextType::class)
+            ->add('prix', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Modifier'))
+            ->getForm();
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em->flush();
+            return $this->redirectToRoute('article_list');
+        }
+        return $this->render('articles/edit.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/article/delete/{id}', name: 'article_delete', methods: ['GET'])]
+    public function delete(Request $request, $id, EntityManagerInterface $em): Response
+    {
+        $article = $em->getRepository(Article::class)->find($id);
+        if (!$article) {
+            throw $this->createNotFoundException('Article non trouvé');
+        }
+
+        $em->remove($article);
+        $em->flush();
+
+        $response = new Response();
+        $response->send();
+        return $this->redirectToRoute('article_list');
     }
 }
