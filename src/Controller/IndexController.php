8ddbfe18eabf947a052cf167ccbc1006e6e06f4f
achrafRejouan <achrafrejouan@gmail.com>
2025-04-11 21:40:28 +0100

    TP3: Refactor IndexController and templates: update route definitions, add articles listing, and create navbar

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -8,25 +8,12 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class IndexController extends AbstractController
 {
-    //Old way of defining routes
-    // #[Route('/', name: 'homepage')]
-    // public function home(): Response
-    // {
-    //     return new Response('<h1>Ma première page Symfony</h1>');
-    // }
-
-    // New way of defining routes using annotations
-    #[Route('/acceuil', name: 'homepage1')]
-    public function home1(): Response
+    #[Route('/', name: 'homepage')]
+    public function home(): Response
     {
-        return $this->render('index.html.twig');
-    }
-
-    #[Route('/acceuil/{name}', name: 'homepage2')]
-    public function home2($name): Response
-    {
-        return $this->render('index1.html.twig', [
-            'prenom' => $name
+        $articles = ['Article 1', 'Article 2', 'Article 3'];
+        return $this->render('articles/index.html.twig', [
+            'articles' => $articles,
         ]);
     }
 }
